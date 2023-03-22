2e56bdd537b9bffcbb3ac8417d6b18254645a00b
SErgioNbP <sergionevespinheiro@gmail.com>
2023-03-22 16:15:23 +0000

    some adjustments added

--- api/server.js
+++ api/server.js
@@ -1,60 +1,68 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://softice:changeme0@cluster0.zvxlzjd.mongodb.net/?retryWrites=true&w=majority', {
-	useNewUrlParser: true, 
-	useUnifiedTopology: true 
-}).then(() => console.log("Connected to MongoDB")).catch(console.error);
+dotenv.config();
 
-// Models
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zvxlzjd.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(console.error);
+
 const Todo = require('./models/Todo');
 
 app.get('/todos', async (req, res) => {
-	const todos = await Todo.find();
+  const todos = await Todo.find();
 
-	res.json(todos);
+  res.json(todos);
 });
 
 app.post('/todo/new', (req, res) => {
-	const todo = new Todo({
-		text: req.body.text
-	})
+  const todo = new Todo({
+    text: req.body.text,
+  });
 
-	todo.save();
+  todo.save();
 
-	res.json(todo);
+  res.json(todo);
 });
 
 app.delete('/todo/delete/:id', async (req, res) => {
-	const result = await Todo.findByIdAndDelete(req.params.id);
+  const result = await Todo.findByIdAndDelete(req.params.id);
 
-	res.json({result});
+  res.json({ result });
 });
 
 app.get('/todo/complete/:id', async (req, res) => {
-	const todo = await Todo.findById(req.params.id);
+  const todo = await Todo.findById(req.params.id);
 
-	todo.complete = !todo.complete;
+  todo.complete = !todo.complete;
 
-	todo.save();
+  todo.save();
 
-	res.json(todo);
-})
+  res.json(todo);
+});
 
 app.put('/todo/update/:id', async (req, res) => {
-	const todo = await Todo.findById(req.params.id);
+  const todo = await Todo.findById(req.params.id);
 
-	todo.text = req.body.text;
+  todo.text = req.body.text;
 
-	todo.save();
+  todo.save();
 
-	res.json(todo);
+  res.json(todo);
 });
 
 app.listen(3001);
